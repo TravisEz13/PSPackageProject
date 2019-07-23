1e97bcc7b4450c5d673224c2cee39ccfca506ab3
Robert Holt <rjmholt@gmail.com>
2019-07-23 10:53:54 -0700

    Add build script

---
+++ build.ps1
@@ -0,0 +1,31 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+param(
+    [switch]
+    $Clean,
+
+    [Parameter()]
+    [string]
+    $OutDirectory = "$PSScriptRoot/out"
+)
+
+$script:ModuleName = "PSPackageProject"
+$script:SrcPath = "$PSScriptRoot/src"
+
+$script:CopyAssets = @{
+    "PSPackageProject.psd1" = "PSPackageProject.psd1"
+    "PSPackageProject.psm1" = "PSPackageProject.psm1"
+}
+
+if ($Clean -and (Test-Path $OutDirectory))
+{
+    Remove-Item -Force -Recurse $OutDirectory -ErrorAction Stop
+}
+
+$outModule = New-Item -ItemType Directory -Path "$OutDirectory/$script:ModuleName"
+foreach ($assetSrcPath in $script:CopyAssets.get_Keys())
+{
+    $destinationPath = Join-Path $outModule $script:CopyAssets[$assetSrcPath]
+    Copy-Item -LiteralPath "$script:SrcPath/$assetSrcPath" $destinationPath
+}
