7d51aee248a9d9d02d5a08120dcf8856265c152a
Robert Holt <rjmholt@gmail.com>
2019-07-23 10:44:04 -0700

    Add first bits

--- test/tasks/PowerShellModuleBuild/common.ts
+++
@@ -1,3 +0,0 @@
-import * as path from 'path';
-
-export const taskIndexPath = path.join(__dirname, '..', '..', '..', 'src', 'tasks', 'PowerShellModuleBuild', 'index.js');
--- test/tasks/PowerShellModuleBuild/failure.ts
+++
@@ -1,9 +0,0 @@
-import * as ma from 'azure-pipelines-task-lib/mock-answer';
-import * as tmrm from 'azure-pipelines-task-lib/mock-run';
-import { taskIndexPath } from './common';
-
-const tmr: tmrm.TaskMockRunner = new tmrm.TaskMockRunner(taskIndexPath);
-
-tmr.setInput('samplestring', 'bad');
-
-tmr.run();
