32779d0fe29497cdf943b804a76c3e5bf1ea44be
Robert Holt <rjmholt@gmail.com>
2019-07-23 15:46:47 -0700

    Add help generation

--- src/PSPackageProject.psm1
+++ src/PSPackageProject.psm1
@@ -1,6 +1,89 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+#region Private implementation functions
+
+$script:pwshPath
+function RunPwshCommandInSubprocess
+{
+    param(
+        [string]
+        $Command
+    )
+
+    if (-not $script:pwshPath)
+    {
+        $script:pwshPath = (Get-Process -Id $PID).Path
+    }
+
+    & $script:pwshPath -NoProfile -NoLogo -Command $Command
+}
+
+function RunProjectBuild
+{
+    param(
+        [string]
+        $ProjectRoot
+    )
+
+    & "$ProjectRoot/build.ps1" -Clean
+}
+
+function GetHelpPath
+{
+    param(
+        [cultureinfo]
+        $Culture,
+
+        [string]
+        $ProjectRoot
+    )
+
+    $ProjectRoot = Resolve-Path -Path $ProjectRoot
+
+    $cultureName = $Culture.Name
+
+    return "$ProjectRoot/help/$cultureName"
+}
+
+function GetOutputModulePath
+{
+    param(
+        [string]
+        $ProjectRoot,
+
+        [string]
+        $ModuleName
+    )
+
+    $ProjectRoot = Resolve-Path -Path $ProjectRoot
+
+    return "$ProjectRoot/out/$ModuleName"
+}
+
+function HasCmdletHelp
+{
+    param(
+        [string]
+        $HelpResourcePath
+    )
+
+    if (-not (Test-Path -Path $HelpResourcePath))
+    {
+        return $false
+    }
+
+    $files = Get-ChildItem -Path $HelpResourcePath -ErrorAction Ignore |
+        Where-Object Name -Like '*.md' |
+        Where-Object Name -NotLike 'about_*'
+
+    return $files.Count -gt 0
+}
+
+#endregion Private implementation functions
+
+#region Public commands
+
 function Invoke-PSPackageProjectTest
 {
     param(
@@ -15,6 +98,7 @@ function Invoke-PSPackageProjectTest
 
 function New-PSPackageProjectHelpStub
 {
+    [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
         [string]
@@ -29,18 +113,77 @@ function New-PSPackageProjectHelpStub
         $Culture = [cultureinfo]::CurrentCulture
     )
 
-    $ProjectRoot = $PSCmdlet.GetUnresolvedProviderPathFromPSPath($ProjectRoot)
+    $ProjectRoot = Resolve-Path -Path $ProjectRoot
 
-    if (-not (Test-Path $ProjectRoot -PathType Container))
-    {
-        throw "Path '$ProjectRoot' is not a valid directory"
-    }
-
-    $cultureName = $Culture.Name
-
-    $helpResourcePath = "$ProjectRoot/help/$cultureName"
+    $helpResourcePath = GetHelpPath -ProjectRoot $ProjectRoot -Culture $Culture 
 
     New-Item -Path $helpResourcePath -ItemType Directory -ErrorAction Stop
 
     New-MarkdownAboutHelp -OutputFolder $helpResourcePath -AboutName $ModuleName
 }
+
+function Add-PSPackageProjectCmdletHelp
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]
+        $ProjectRoot,
+
+        [Parameter(Mandatory)]
+        [string]
+        $ModuleName,
+
+        [Parameter()]
+        [cultureinfo]
+        $Culture,
+
+        [switch]
+        $Force
+    )
+
+    $ProjectRoot = Resolve-Path -Path $ProjectRoot
+
+    $helpResourcePath = GetHelpPath -ProjectRoot $ProjectRoot -Culture $Culture
+
+    RunProjectBuild -ProjectRoot $ProjectRoot
+
+    $outModulePath = GetOutputModulePath -ProjectRoot $ProjectRoot -ModuleName $ModuleName
+
+    if (-not (HasCmdletHelp -HelpResourcePath $helpResourcePath))
+    {
+        New-Item -Path $helpResourcePath -ItemType Directory
+        RunPwshCommandInSubprocess -Command "Import-Module '$outModulePath'; New-MarkdownHelp -Module $ModuleName -OutputFolder '$helpResourcePath'"
+        return
+    }
+
+    RunPwshCommandInSubprocess -Command "Import-Module '$outModulePath'; Update-MarkdownHelp -Path '$helpResourcePath'"
+}
+
+function Publish-PSPackageProjectHelp
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]
+        $ProjectRoot,
+
+        [Parameter(Mandatory)]
+        [string]
+        $ModuleName,
+
+        [Parameter()]
+        [cultureinfo]
+        $Culture = [cultureinfo]::CurrentCulture
+    )
+
+    $cultureName = $Culture.Name
+
+    $helpResourcePath = GetHelpPath -ProjectRoot $ProjectRoot -Culture $Culture
+
+    $outModulePath = GetOutputModulePath -ProjectRoot $ProjectRoot -ModuleName $ModuleName
+
+    New-ExternalHelp -Path $helpResourcePath -OutputPath "$outModulePath/$cultureName" -Force
+}
+
+#endregion Public commands
